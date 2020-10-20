ddfd2ba825ec7d54a68eb6c7bf5fb979c403d7ca
Pieter <32438581+pietermartens@users.noreply.github.com>
2020-10-20 11:11:09 +0200

    Update config.php

--- config.php
+++ config.php
@@ -1,10 +1,10 @@
 <?php
     require('vendor/autoload.php');
     // Define mysql server settings
-    $serverName = "localhost";
-    $username = "root";
-    $password = "";
-    $dbName = "employees";
+    $serverName = "sqldatabase.cf5ctuboq7jl.us-east-1.rds.amazonaws.com";
+    $username = "admin";
+    $password = "Cloud2020";
+    $dbName = "testData";
 
     // Create a new mysqli connection. Remember to enable this in php.ini !!
     $conn = new mysqli($serverName, $username, $password, $dbName);

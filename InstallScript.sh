c73a626043f275c6929f942208925b16fe19dc18
Pieter <32438581+pietermartens@users.noreply.github.com>
2020-10-26 00:57:02 +0100

    Update InstallScript.sh

--- InstallScript.sh
+++ InstallScript.sh
@@ -1,10 +1,3 @@
 sudo su
 sudo apt update
 sudo apt install wget php-cli php-zip unzip
-sudo apt install php libapache2-mod-php
-sudo systemctl restart apache2
-sudo apt update && sudo apt install wget php-cli php-zip unzip curl
-curl -sS https://getcomposer.org/installer |php
-sudo mv composer.phar /usr/local/bin/composer
-composer install
-

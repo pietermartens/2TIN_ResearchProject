8a4ac481476ca6e930cc1c9a6bbe761e00b56a74
Pieter <32438581+pietermartens@users.noreply.github.com>
2020-10-26 01:21:28 +0100

    Update CleanupScript.sh

--- CleanupScript.sh
+++ CleanupScript.sh
@@ -3,5 +3,6 @@ rm index.html
 sudo apt install php libapache2-mod-php
 sudo systemctl restart apache2
 sudo apt install -y wget php-cli php-zip unzip
+sudo apt-get install -y php-xml
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 sudo php composer-setup.php --install-dir=/usr/local/bin --filename=composer

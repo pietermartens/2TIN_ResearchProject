ecad108728c7355c9be9b5011fa0aa6730d3c154
Pieter <32438581+pietermartens@users.noreply.github.com>
2020-10-27 13:46:36 +0100

    Update Composer.sh

--- Composer.sh
+++ Composer.sh
@@ -3,6 +3,9 @@ export COMPOSER_HOME="$HOME/.config/composer"
 cd /var/www/html
 sudo su
 composer install
+ansible-galaxy collection install community.aws
+sudo apt install -y python3-pip
+python3 -m pip install boto3
 sudo chmod 777 /var/www/html
 cd /home/ubuntu
 ansible-playbook RDS.yml 

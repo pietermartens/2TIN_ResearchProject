ec0a60fbabb2a939f2cc28d5d5ad135acff5b62c
Pieter <32438581+pietermartens@users.noreply.github.com>
2020-10-27 17:05:08 +0100

    Update Composer.sh

--- Composer.sh
+++ Composer.sh
@@ -1,6 +1,7 @@
 sudo apt-get install -y composer
 export COMPOSER_HOME="$HOME/.config/composer"
 sudo su
+ansible-galaxy collection install community.mysql
 ansible-galaxy collection install community.aws
 sudo apt install -y python3-pip
 python3 -m pip install boto3

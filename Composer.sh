c2d8ff1092adb3fa8aa894e99196d055d2fe270c
Pieter <32438581+pietermartens@users.noreply.github.com>
2020-12-13 14:26:44 +0100

    Update Composer.sh

--- Composer.sh
+++ Composer.sh
@@ -1,13 +1,5 @@
-sudo apt-get install -y composer
-sudo apt-get install -y mysql-server
 export COMPOSER_HOME="$HOME/.config/composer"
-sudo su
-sudo apt install -y python3-pip
-python3 -m pip install boto3
-sudo chmod 777 /var/www/html
 cd /home/ubuntu
-ansible-galaxy collection install community.mysql
-ansible-galaxy collection install community.aws
 ansible-playbook RDS.yml 
 cd /var/www/html
 composer install

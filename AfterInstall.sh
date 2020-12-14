d74f8f39f7e56dfd0d08651dcf43ae0338fb0f45
Pieter <32438581+pietermartens@users.noreply.github.com>
2020-12-14 11:23:08 +0100

    Update AfterInstall.sh

--- AfterInstall.sh
+++ AfterInstall.sh
@@ -1,6 +1,5 @@
 export COMPOSER_HOME="$HOME/.config/composer"
 cd /home/ubuntu
-ls
 ansible-galaxy collection install community.mysql
 ansible-galaxy collection install community.aws
 ansible-playbook RDS.yml

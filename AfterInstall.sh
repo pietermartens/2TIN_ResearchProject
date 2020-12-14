87414c57b54db6f4364f9158bb2206911594bdfd
Pieter <32438581+pietermartens@users.noreply.github.com>
2020-12-14 12:44:21 +0100

    Update AfterInstall.sh

--- AfterInstall.sh
+++ AfterInstall.sh
@@ -2,6 +2,8 @@ cd /home/ubuntu
 ansible-galaxy collection install community.mysql
 ansible-galaxy collection install community.aws
 ansible-playbook WebServer.yml
+
 cd /var/www/html
+export COMPOSER_HOME="$HOME/.config/composer"
 composer install
 
